8a153c773e4b561f3f871621eccea10340995890
Lowe_Marklund <lm222qb@student.lnu.se>
2018-09-27 12:01:11 +0200

    Created field and constructior for the member class

--- controller/Member.cs
+++ controller/Member.cs
@@ -10,67 +10,9 @@ namespace controller
     /// </summary>
     class Member
     {
-        //The Members name.
-        private string _name;
-
-        //The Members unique id.
-        private int _id;
-
-        //The Members personal number.
-        private int _number;
-
-        //private List<Boat> _boats = new List<Boat>();
-
-        public string Name {
-            get{
-               return _name;
-            }
-
-            private set{
-                _name = value;
-            }
+        static void Main(string[] args)
+        {
+        
         }
-
-        public int Id {
-            get{
-               return _id;
-            }
-
-            private set{
-                _id = value;
-            }
-        }
-
-        public int Number {
-            get{
-               return _number;
-            }
-
-            private set{
-                _number = value;
-            }
-        }
-
-        /// <summary>
-        /// Creates an instance if the Member class.
-        /// </summary>
-        public Member(string name, int id, int number){
-            Name = name;
-            Id = id; 
-            Number = number;
-        }
-
-        /// <summary>
-        /// Creates a string containing the member's information.
-        /// </summary>
-        /// <returns>
-        /// A string containing the member's information.
-        /// </returns>
-        public override string ToString(){
-            string memberInfoString = $"{this.Name}\n{this.Id}\n{this.Number}\n";
-
-            return memberInfoString;
-        }
-
     }
 }
